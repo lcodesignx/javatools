5231a6d0530f092154a875715f8d8fb4475d5108
Alvaro Lupercio <lupercio0184@gmail.com>
2020-02-08 10:58:50 -0500

    changed SingletonDesignDemo to TestSingletonPattern

--- workspace/DesignPatterns/src/singletonDesignPattern/TestSingletonPattern.java
+++ workspace/DesignPatterns/src/singletonDesignPattern/TestSingletonPattern.java
@@ -2,7 +2,7 @@ package singletonDesignPattern;
 
 import java.sql.SQLException;
 
-public class SingletonDesignDemo {
+public class TestSingletonPattern {
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
